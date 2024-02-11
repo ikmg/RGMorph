8032c2dd7ba239a083ff944ca9e56aedce057429
KMG <128629445+ikmg@users.noreply.github.com>
2024-02-11 14:36:17 +0300

    Delete unit_test directory

--- unit_test/morph.py
+++
@@ -1,7 +0,0 @@
-from rg_morph import Morph
-
-
-morph = Morph()
-print(morph.fio('родительный', middlename='Георгиевич', to_string=True))
-print(morph.phrase('родительный', 'ФГКУ "Главный центр Оршанско-Хинганский Федерации"', True))
-q=1
